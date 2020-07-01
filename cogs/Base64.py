2163aeca36c532bc993655fc94f1bb291f2b7f05
TheUpBeat <TheUpBeat@protonmail.com>
2020-07-01 23:25:18 +0530

    Removed event. Added encode command

--- cogs/Base64.py
+++ cogs/Base64.py
@@ -1,4 +1,3 @@
-import os
 import base64
 import discord
 from discord.ext import commands
@@ -8,26 +7,20 @@ class Base64(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.channel.id == "id":
-            msg = message.content
-            channel = message.channel
-            msg_bytes = msg.encode('ascii')
-            base64_bytes = base64.b64encode(msg_bytes)
-            base64_msg = base64_bytes.decode('ascii')
+    @commands.command()
+    async def encode(self, ctx, Title, *, msg):
+        msg_bytes = msg.encode('ascii')
+        base64_bytes = base64.b64encode(msg_bytes)
+        base64_msg = base64_bytes.decode('ascii')
 
-            embed = discord.Embed(
-                colour = discord.Colour.green()
-            )
+        embed = discord.Embed(
+            colour = discord.Colour.green(),
+            title = 'Base64 Encoded'
+        )
 
-            embed.add_field(name = 'Base64Encoded', value = base64_msg, inline = False)
-
-            try:
-                await channel.send(embed=embed)
-                await message.delete()
-            except Exception as e:
-                print(e)
+        embed.add_field(name = Title, value = base64_msg, inline = False)
+        await ctx.send(embed = embed)
+        await ctx.message.delete()
 
     @commands.command()
     async def decode(self, ctx, *, msg):
@@ -36,7 +29,8 @@ class Base64(commands.Cog):
         base64_msg = base64_bytes.decode('ascii')
 
         embed_1 = discord.Embed(
-            colour = discord.Colour.blue()
+            colour = discord.Colour.blue(),
+            title = 'Base64 Decoded'
         )
 
         embed_1.add_field(name = 'Decoded Message', value = base64_msg, inline = False)
